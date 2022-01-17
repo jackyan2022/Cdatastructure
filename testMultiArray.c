760c47226fbfb3081ad28882cbeb58e42dae1b46
yan zhong <jackyan63@gmail.com>
2022-01-16 21:44:43 -0500

    multiarray and sizeof

---
+++ testMultiArray.c
@@ -0,0 +1,34 @@
+	#include <stdio.h>
+
+	int main() {
+		/* TODO: declare the 2D array grades here */
+		float average;
+		int i;
+		int j;
+        int grades[2][5];
+		grades[0][0] = 80;
+		grades[0][1] = 70;
+		grades[0][2] = 65;
+		grades[0][3] = 89;
+		grades[0][4] = 90;
+
+		grades[1][0] = 85;
+		grades[1][1] = 80;
+		grades[1][2] = 80;
+		grades[1][3] = 82;
+		grades[1][4] = 87;
+
+		/* TODO: complete the for loop with appropriate terminating conditions */
+		for (i = 0; i < sizeof(grades) / sizeof(grades[i]); i++) {
+			average = 0;
+			for (j = 0; j < sizeof(grades[0]) / 4; j++) {
+				average += grades[i][j];
+			}
+			printf("size of grade 0 %d\n", (int) sizeof(grades));
+
+			/* TODO: compute the average marks for subject i */
+			printf("The average marks obtained in subject %d is: %.2f\n", i, average);
+		}
+
+		return 0;
+	}
